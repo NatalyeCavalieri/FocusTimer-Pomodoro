2a01da2b031bbef1cc7a7f85a02bcf8881126d37
Natalye Cavalieri <nahcavalieri@gmail.com>
2023-09-11 14:14:44 -0300

    init focus timer

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,8 @@
+export const buttonPressAudio = new Audio('./assets/buttonpress.mp3.wav')
+
+export const alarm = new Audio('./assets/alarm.mp3')
+
+
+export const human = new Audio ('./assets/human.mp3')
+
+human.loop = true
